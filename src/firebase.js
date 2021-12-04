c17576238298d722510ce5f8b7025d94813e610d
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-12-04 16:58:19 +0530

    cleaned up the basic stuff and added firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDVx55C9u2k6LUc5cqjzYoqp3Tu85LBHHc",
+  authDomain: "clone-b682f.firebaseapp.com",
+  projectId: "clone-b682f",
+  storageBucket: "clone-b682f.appspot.com",
+  messagingSenderId: "151468078920",
+  appId: "1:151468078920:web:6ace2a62129bdc4ec0e57d",
+};
